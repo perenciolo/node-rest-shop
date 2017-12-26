d9f2ee0a1d6df99f5d7803dacc7ee3fef2714e6a
Gustavo Perenciolo <gustavoperenciolo@MacBook-Pro-de-Gustavo.local>
2017-12-26 16:59:50 -0200

    Adding img support to the route products

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,31 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const multer = require('multer')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname)
+  }
+})
+const fileFilter = (req, file, cb) => {
+  // accept file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'imagep/png' || file.mimetype === 'image/gif') {
+    cb(null, true)
+  }else {
+    // reject file
+    cb(new Error('File not supported.'), false)
+  }
+}
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+})
 
 const Product = require('../models/product')
 
@@ -34,7 +59,9 @@ router.get('/', (req, res, next) => {
     })
 })
 
-router.post('/', (req, res, next) => {
+// POST /products -- ADDING NEW PRODUCTS
+router.post('/', upload.single('productImage'), (req, res, next) => {
+  console.log(req.file)
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
